988a77aaa434c44ab5ace73bd40b45c03640152e
Maris Orbidans <MarisOrbidans@thisisnoble.com>
2016-05-02 04:04:32 +0100

    initial commit

---
+++ chapter5.hs
@@ -0,0 +1,31 @@
+module Main where
+
+main :: IO() ; main = undefined
+
+curry' :: ((a, b) -> c) -> a -> b -> c
+curry' f a b = f (a, b)
+
+uncurry' :: (a -> b -> c) -> (a, b) -> c
+uncurry' f (a,b) = f a b
+
+f1 :: a -> a -> a-> a ; f1 = undefined
+x :: Char; x = undefined
+-- :t f1 x
+
+g :: a -> b -> c -> b ; g = undefined
+-- :t g 0 'c' "foo"
+
+h :: (Num a, Num b) => a -> b -> b ; h = undefined
+-- :t h 1.0 2
+
+j :: (Num a, Num b) => a -> b -> b ; j = undefined
+-- :t j 1 (5.5 :: Double)
+
+jackal :: (Ord a, Eq b) => a -> b -> a ; jackal = undefined
+-- :t jackal "foo" "bar"
+-- :t jackal "foo"
+
+kessel :: (Ord a, Num b) => a -> b -> a ; kessel = undefined
+-- :t kessel 1 2
+-- :t kessel 1 (2 :: Integer)
+-- :t kessel (1 :: Integer) 2
