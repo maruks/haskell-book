b07c5250ae53c2ee07393a92a89314606860c0df
Maris Orbidans <maris.orbidans@agentsmutual.co.uk>
2016-07-17 22:44:54 +0100

    exercises

--- Chapter9_lists.hs
+++ Chapter9_lists.hs
@@ -50,3 +50,47 @@ unCaesar c = chr ( 97 + ( (ord c) - 102) `mod` 26 )
 
 cipherText :: String -> String
 cipherText = map caesar
+
+myOr :: [Bool] -> Bool
+myOr [] = False
+myOr (x:xs) = if x then x else myOr xs
+
+myAny :: (a -> Bool) -> [a] -> Bool
+myAny _ [] = False
+myAny p (x:xs) = if p x then True else myAny p xs
+
+myElem :: Eq a => a -> [a] -> Bool
+myElem _ [] = False
+myElem a (x:xs) = if a == x then True else myElem a xs
+
+myElem2 :: Eq a => a -> [a] -> Bool
+myElem2 a = myAny (\x -> x == a)
+
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse (x:xs) = myReverse xs ++ [x]
+
+squish :: [[a]] -> [a]
+squish [] = []
+squish (x:xs) = x ++ squish xs
+
+
+squishMap :: (a -> [b]) -> [a] -> [b]
+squishMap _ [] = []
+squishMap f (x:xs) = (f x) ++ squishMap f xs
+
+squishAgain :: [[a]] -> [a]
+squishAgain = squishMap id
+
+myMaxBy :: (a -> a -> Ordering) -> [a] -> a
+myMaxBy _ [] = error "empty list"
+myMaxBy _ [x] = x
+myMaxBy f (x:xs) = if f x t == GT then x else t
+  where t = myMaxBy f xs
+
+
+myMinBy :: (a -> a -> Ordering) -> [a] -> a
+myMinBy _ [] = error "empty list"
+myMinBy _ [x] = x
+myMinBy f (x:xs) = if f x t == LT then x else t
+  where t = myMinBy f xs
