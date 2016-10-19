3894d1fe5f6fc9d114eddd5a98ace6e40fb9659c
Maris Orbidans <maris.orbidans@agentsmutual.co.uk>
2016-10-19 01:47:18 +0100

    reformat

--- src/Chapter11_adt.hs
+++ src/Chapter11_adt.hs
@@ -2,17 +2,25 @@ module Chapter11_adt where
 
 -- binary tree
 
-data BinaryTree a = Leaf
-                  | Node (BinaryTree a) a (BinaryTree a) deriving (Eq, Ord, Show)
+data BinaryTree a
+  = Leaf
+  | Node (BinaryTree a)
+         a
+         (BinaryTree a)
+  deriving (Eq, Ord, Show)
 
-insert' :: Ord a => a -> BinaryTree a -> BinaryTree a
+insert'
+  :: Ord a
+  => a -> BinaryTree a -> BinaryTree a
 insert' b Leaf = Node Leaf b Leaf
 insert' b (Node left a right)
   | b == a = Node left a right
-  | b<a = Node(insert' b left) a right
-  | b>a = Node left a (insert' b right)
+  | b < a = Node (insert' b left) a right
+  | b > a = Node left a (insert' b right)
 
-fromList :: Ord a => [a] -> BinaryTree a
+fromList
+  :: Ord a
+  => [a] -> BinaryTree a
 fromList = foldr insert' Leaf
 
 inorder :: BinaryTree a -> [a]
