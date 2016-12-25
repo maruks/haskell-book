5039783398c01f70487c6daf18292e8e0816366c
Maris Orbidans <maris.orbidans@agentsmutual.co.uk>
2016-12-25 23:57:30 +0000

    reader

---
+++ src/Chapter22_reader.hs
@@ -0,0 +1,8 @@
+module Chapter18_reader where
+
+import Control.Monad
+
+import Test.QuickCheck hiding (Failure, Success)
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
+import Test.QuickCheck.Function
