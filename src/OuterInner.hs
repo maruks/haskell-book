4cea0d964dca0768e6e06976ff06bf42d6044741
Maris Orbidans <maris.orbidans@agentsmutual.co.uk>
2017-01-26 23:54:23 +0000

    OuterInner

--- src/OuterInner.hs
+++ src/OuterInner.hs
@@ -17,6 +17,9 @@ eitherUnwrap = runExceptT maybeUnwrap
 readerUnwrap :: () -> IO (Either String (Maybe Int))
 readerUnwrap = runReaderT eitherUnwrap
 
+-- readerUnwrap ()
+-- Right (Just 1)
+
 embedded2 :: MaybeT (ExceptT String (ReaderT () IO)) Int
 embedded2 = let i:: ReaderT () IO (Either String (Maybe Int))
                 i = ReaderT (const $ return (Right (Just 1)))
